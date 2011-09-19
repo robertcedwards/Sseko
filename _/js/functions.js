0502e05d065c014b93b779a7f6db708ac56de3c5
robertcedwards <robert.pastorella@gmail.com>
2011-09-19 16:55:00 -0700

    first commit

---
+++ _/js/functions.js
@@ -0,0 +1,23 @@
+// remap jQuery to $
+(function($){})(window.jQuery);
+
+
+/* trigger when page is ready */
+$(document).ready(function (){
+
+    $.backstretch("wp-content/themes/Sseko/images/bg.jpg", {centeredX: false}, {centeredY: false});
+
+});
+
+
+/* optional triggers
+
+$(window).load(function() {
+	
+});
+
+$(window).resize(function() {
+	
+});
+
+*/

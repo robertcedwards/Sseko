dec83f87cc77f6d3af63113c2f4efd37d66f8acf
Robert Edwards <robertcedwards@coactlab.com>
2011-09-23 16:46:14 -0700

    foxix

--- header.php
+++ header.php
@@ -155,7 +155,7 @@ var ADAPT_CONFIG = {
 				<img src="<?php bloginfo('template_directory'); ?>/images/description.png"/>
 			</div>
 					 	
-		 	<div class="grid_8nav">
+		 	<div class="grid_8 nav">
 				<?php wp_nav_menu( array( 'sort_column' => 'menu_order', 'container_class' => 'menu' ) ); ?>
 		 	</div>
 			<div class="clear"></div> 

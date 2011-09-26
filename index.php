27e2d572db2412beb646ee9d175c5e9c0af1c939
Robert Edwards <robertcedwards@coactlab.com>
2011-09-26 16:21:48 -0700

    Mailchimp addition

--- index.php
+++ index.php
@@ -39,24 +39,22 @@ Template Name: Home Page
 								<li class="grid_3 mini"><a href="<?php echo get('mini_featured_link',1,2); ?>"><img src="<?php echo get('mini_featured_image',1,2); ?>"/></a></li>
 								<li class="grid_3 mini"><a href="<?php echo get('mini_featured_link',1,3); ?>"><img src="<?php echo get('mini_featured_image',1,3); ?>"/></a></li>
 								<li class="grid_3 mini">
-								<!-- Begin MailChimp Signup Form -->
-									<div id="mc_embed_signup">
-									<form action="http://ssekodesigns.us2.list-manage1.com/subscribe/post?u=918f13b2195c1a617550a626a&amp;id=b87be29ca1" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank">
-									<h2>Subscribe to our mailing list</h2>
-									<div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
-										<label for="mce-EMAIL">Email Address  <span class="asterisk">*</span></label>
-										<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
-									</div>
-									<div id="mce-responses" class="clear">
-										<div class="response" id="mce-error-response" style="display:none"></div>
-										<div class="response" id="mce-success-response" style="display:none"></div>
-									</div>
-									<div class="clear">
-									<input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-									</form>
-									</div>
-<!--End mc_embed_signup-->
-								</li>
+<!-- Begin MailChimp Signup Form -->
+<div id="mc_embed_signup">
+<form action="http://ssekodesigns.us2.list-manage.com/subscribe/post?u=918f13b2195c1a617550a626a&amp;id=b87be29ca1" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank">
+	
+<div class="mc-field-group">
+	<label for="mce-EMAIL">Email Address </label>
+	<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+</div>
+	<div id="mce-responses" class="clear">
+		<div class="response" id="mce-error-response" style="display:none"></div>
+		<div class="response" id="mce-success-response" style="display:none"></div>
+	</div>	<div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+</form>
+</div>
+
+<!--End mc_embed_signup-->								</li>
 							</ul>
 						
 						<div class="clear"></div>	

c249e5a9b2119c5a8aa5c0f9617f417604f5b83d
Robert Edwards <robertcedwards@coactlab.com>
2011-09-30 17:41:23 -0700

    updated wording for the date

--- _/inc/meta.php
+++ _/inc/meta.php
@@ -2,7 +2,7 @@
 	<span class="byline author vcard">
 		<i>posted by</i> <span class="fn"><?php the_author() ?> </span>
 	</span>
-	<i>on </i> <time datetime="<?php echo date(DATE_W3C); ?>" pubdate class="updated"><?php the_time('l, F j') ?></time>
+	<i>on </i> <time datetime="<?php echo date(DATE_W3C); ?>" pubdate class="updated"><?php the_time('l, F j') ?> in</time>
 	<i><?php the_category(', ') ?></i>
 	<?php comments_popup_link('No Comments', '1 Comment', '% Comments', 'comments-link', ''); ?>
 </footer>

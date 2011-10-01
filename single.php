9bfd2f9b02f22d8213ced418f0e6ef4c4e54cafe
Rob Edwards - iMac <robertcedwards@coactlab.com>
2011-10-01 10:58:53 -0700

    fixing

--- single.php
+++ single.php
@@ -1,19 +1,29 @@
 <?php get_header(); ?>
  
-	<div id="content" class="grid_8"> 
+	<div id="content" class="grid_8">
+
+        <?php query_posts('post_type=post&post_status=publish&posts_per_page=10&paged='. get_query_var('paged')); ?>
+ 
+	<?php if( have_posts() ): ?>
+ 
 	    <div id="post-<?php get_the_ID(); ?>" <?php post_class('grid_8'); ?>>
  
-            <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-            <span class="entry-meta"><?php include (TEMPLATEPATH . '/_/inc/meta.php' ); ?></span>
-	       	<hr class="grid_4">
- 			<div class="clear"></div>
- 			<?php the_content(); ?>
- 			<span class="comments"><?php comments_popup_link(__('0 comments','example'),__('1 comment','example'),__('% comments','example')); ?></span>
-        
+ 
+                <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                <span class="entry-meta"><?php include (TEMPLATEPATH . '/_/inc/meta.php' ); ?></span>
+	        	<hr class="grid_4">
+ 				<div class="clear"></div>
+ 
+               
+ 
+				<?php the_content(); ?>
+  		<span class="comments"><?php comments_popup_link(__('0 comments','example'),__('1 comment','example'),__('% comments','example')); ?></span>
         </div><!-- /#post-<?php get_the_ID(); ?> -->
         <hr class="grid_8">
  		<div class="clear"></div>
+        <?php endwhile; ?>
 
+	<?php else: ?>
  
 		<div id="post-404" class="noposts">
  
@@ -21,6 +31,7 @@
  
 	    </div><!-- /#post-404 -->
  
+	<?php endif; wp_reset_query(); ?>
  
 	</div><!-- /#content -->
  	<?php get_sidebar(); ?>

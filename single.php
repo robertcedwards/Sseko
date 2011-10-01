a6fd3624b378d2c31441d9c4072949902c476007
Rob Edwards - iMac <robertcedwards@coactlab.com>
2011-10-01 11:07:51 -0700

    updates to single

--- single.php
+++ single.php
@@ -2,26 +2,20 @@
  
 	<div id="content" class="grid_8">
 
-        <?php query_posts('post_type=post&post_status=publish&posts_per_page=10&paged='. get_query_var('paged')); ?>
  
-	<?php if( have_posts() ): ?>
- 
-	    <div id="post-<?php get_the_ID(); ?>" <?php post_class('grid_8'); ?>>
- 
- 
-                <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-                <span class="entry-meta"><?php include (TEMPLATEPATH . '/_/inc/meta.php' ); ?></span>
+	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+ 		<article <?php post_class('grid_8') ?> id="post-<?php the_ID(); ?>">
+
+            <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+            	<span class="entry-meta"><?php include (TEMPLATEPATH . '/_/inc/meta.php' ); ?></span>
 	        	<hr class="grid_4">
- 				<div class="clear"></div>
- 
-               
- 
-				<?php the_content(); ?>
-  		<span class="comments"><?php comments_popup_link(__('0 comments','example'),__('1 comment','example'),__('% comments','example')); ?></span>
-        </div><!-- /#post-<?php get_the_ID(); ?> -->
-        <hr class="grid_8">
- 		<div class="clear"></div>
- 		
+	        <div class="clear"></div>
+			
+			<?php the_content(); ?>
+        	<hr class="grid_8">
+ 			<div class="clear"></div>
+ 		</article>
+		<?php comments_template(); ?>
 	<?php else: ?>
  
 		<div id="post-404" class="noposts">
@@ -30,8 +24,8 @@
  
 	    </div><!-- /#post-404 -->
  
-	<?php endif; wp_reset_query(); ?>
- 
+	<?php endwhile; endif; ?>
+
 	</div><!-- /#content -->
  	<?php get_sidebar(); ?>
  	

82c51184a7ca69817f5e9efc24b39b336f99b420
Robert Edwards <robertcedwards@coactlab.com>
2011-09-30 17:30:01 -0700

    updates to order of meta files

--- _/inc/meta.php
+++ _/inc/meta.php
@@ -1,7 +1,8 @@
 <footer class="meta">
-	<i>Posted on:</i> <time datetime="<?php echo date(DATE_W3C); ?>" pubdate class="updated"><?php the_time('F jS, Y') ?></time>
 	<span class="byline author vcard">
-		<i>by</i> <span class="fn"><?php the_author() ?></span>
+		<i>posted by</i> <span class="fn"><?php the_author() ?> </span>
 	</span>
+	<i>on </i> <time datetime="<?php echo date(DATE_W3C); ?>" pubdate class="updated"><?php the_time('l, F j, ') ?></time>
+	<i><?php the_category(', ') ?></i>
 	<?php comments_popup_link('No Comments', '1 Comment', '% Comments', 'comments-link', ''); ?>
 </footer>

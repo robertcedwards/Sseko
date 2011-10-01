08ef587d1f4d7071db5f5e0d372c2ae944befe50
Robert Edwards <robertcedwards@coactlab.com>
2011-10-01 06:37:00 -0700

    adding hr

--- news.php
+++ news.php
@@ -23,7 +23,8 @@ Template Name: News
 				<?php the_content(); ?>
   		<span class="comments"><?php comments_popup_link(__('0 comments','example'),__('1 comment','example'),__('% comments','example')); ?></span>
         </div><!-- /#post-<?php get_the_ID(); ?> -->
- 		
+ 		<hr class="grid_8">
+ 		<div class="clear">
         <?php endwhile; ?>
     
 	<?php else: ?>

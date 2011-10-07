6259f6286384e7338bdee75d6e7caaabf7e348d2
Rob Edwards - iMac <robertcedwards@coactlab.com>
2011-10-07 14:42:08 -0700

    fancybox filxes

--- header.php
+++ header.php
@@ -118,11 +118,11 @@ var ADAPT_CONFIG = {
 <script src="<?php bloginfo('template_directory'); ?>/_/js/modernizr-1.7.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
 <script src="<?php bloginfo('template_directory'); ?>/_/js/jquery.blueberry.js"></script>
-<script>$(window).load(function() {$('.blueberry').blueberry();});</script>
-<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/_/js/fancybox/jquery.fancybox-1.3.4.pack.js"></script>
-<script>	$("a.inline").fancybox({
+<script>$(window).load(function() {$('.blueberry').blueberry();};$('a.inline').fancybox({
 		'hideOnContentClick': true
-	});</script>
+	}););</script>
+<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/_/js/fancybox/jquery.fancybox-1.3.4.pack.js"></script>
+<script>	</script>
 
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 

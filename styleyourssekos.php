5617d9209226c303bb11040458d021d3cfd80470
Rob Edwards - Macbook Air <robertcedwards@coactlab.com>
2011-10-10 08:41:21 -0700

    adding video embedding

--- styleyourssekos.php
+++ styleyourssekos.php
@@ -13,27 +13,13 @@ Template Name: Style your Ssekos
 									if(is_array($womens))
 									{foreach($womens as $women)
 										{
-										 echo "<h2>$women</h2>"; 
-										 echo "<a href='"; 
-										 echo get('link',1,$women); 
-										 echo "'class='grid_3 inline'>";
-										 echo "<img class='grid_2'src='";
-										 echo get('image',1,$women);
-										 echo "'/></a>";
+										 echo "<h2>";
+										 echo get('name',1,$women); 
+										 echo "<a href='#info$women'class='grid_3 inline'>"; 
+										 echo get_image('link',1,$women); 
+										 echo "</a>";
 										 echo "<div style='display:none'><div id='info$women' class='grid_8'>";
-										 echo "<img class='grid_2'src='";
-										 echo get('image',1,$women);
-										 echo "'/>";
-										 echo "<ul class='grid_6'><li><strong>From:</strong>";
-										 echo get('from',1,$women);
-										 echo "</li>";
-										 echo "<li><strong>Educational Goals:</strong>";
-										 echo get('educational_goals',1,$women);
-										 echo "</li>";
-										 echo "<li><strong>Message to the Sseko Family:</strong>";
-										 echo get('message',1,$women);
-										 echo "</li>";
-										 echo "</ul>";
+										 echo get('link',1,$women);
 										 echo "</div></div>";
 
 										}

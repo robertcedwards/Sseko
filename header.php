ae6b3c95a95710c197f724dc4bab3fe5b1837c48
Rob Edwards - Macbook Air <robertcedwards@coactlab.com>
2011-10-10 11:39:51 -0700

    changes namespace

--- header.php
+++ header.php
@@ -125,7 +125,7 @@ var ADAPT_CONFIG = {
   <script>
     (function($){
       
-      $('.nav').stickyNav({marginTop: 25, });
+      $('#nav').stickyNav({marginTop: 25, });
       
     })(jQuery);
   </script>
@@ -152,7 +152,7 @@ var ADAPT_CONFIG = {
 				<img src="<?php bloginfo('template_directory'); ?>/images/description.png"/>
 			</div>
 					 	
-		 	<div class="grid_9 nav">
+		 	<div id="sticky" class="grid_9 nav">
 				<?php wp_nav_menu( array( 'sort_column' => 'menu_order', 'container_class' => 'menu' ) ); ?>
 		 	</div>
 			<div class="clear"></div> 

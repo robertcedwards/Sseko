0f0e283813c6f99a8938a595ba62afb093bbeeec
Rob Edwards - Macbook Air <robertcedwards@coactlab.com>
2011-10-10 11:37:57 -0700

    fixed javascript link

--- header.php
+++ header.php
@@ -117,7 +117,7 @@ var ADAPT_CONFIG = {
 <script src="<?php bloginfo('template_directory'); ?>/_/js/adapt.min.js"></script>
 <script src="<?php bloginfo('template_directory'); ?>/_/js/modernizr-1.7.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
-<script src="_/js/jquery.stickysidebar.js"></script>
+<script src="<?php bloginfo('template_directory'); ?>/_/js/jquery.stickysidebar.js"></script>
 <script src="<?php bloginfo('template_directory'); ?>/_/js/jquery.blueberry.js"></script>
 <script>$(window).load(function() {$('.blueberry').blueberry();});</script>
 <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/_/js/fancybox/jquery.fancybox-1.3.4.pack.js"></script>

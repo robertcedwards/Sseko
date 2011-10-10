9c837d5842051d9113ac92530d80c42e97f4b81d
Robert C Edwards <redwards@coactlab.com>
2011-10-10 15:53:38 -0700

    fixing the javascript

--- header.php
+++ header.php
@@ -121,11 +121,8 @@ var ADAPT_CONFIG = {
 <script src="<?php bloginfo('template_directory'); ?>/_/js/jquery.blueberry.js"></script>
 <script>$(window).load(function() {$('.blueberry').blueberry();});</script>
 <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/_/js/fancybox/jquery.fancybox-1.3.4.pack.js"></script>
-<script type="text/javascript">$(document).ready(function() 
-{$('a.inline').fancybox({'hideOnContentClick': true});}
-{$('.nav').jsticky({marginTop: 25, });}
-);
-</script>
+<script type="text/javascript">$(document).ready(function() {$("a.inline").fancybox({'hideOnContentClick': true});});</script>
+<script>$(document).ready(function() {$('.nav').jsticky({marginTop: 25, });});</script>
 
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 

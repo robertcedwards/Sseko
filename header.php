a33e1adf0f3927d5cf02c20125d2b38637d1505c
Rob Edwards - Macbook Air <robertcedwards@coactlab.com>
2011-10-10 11:40:42 -0700

    sticky

--- header.php
+++ header.php
@@ -125,7 +125,7 @@ var ADAPT_CONFIG = {
   <script>
     (function($){
       
-      $('#nav').stickyNav({marginTop: 25, });
+      $('#sticky').stickyNav({marginTop: 25, });
       
     })(jQuery);
   </script>

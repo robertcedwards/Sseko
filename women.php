d1a6c725a95ada129ce97e7b00b88ae20e9c22db
Robert C Edwards <redwards@coactlab.com>
2011-10-11 14:15:12 -0700

    added rel="women" to chain lightboxes together

--- women.php
+++ women.php
@@ -13,7 +13,7 @@ Template Name: Women of Sseko
 				<?php $womens = getFieldOrder('image'); 
 									if(is_array($womens))
 									{foreach($womens as $women)
-										{echo "<a href='#info$women'class='grid_3 inline'>"; 
+										{echo "<a rel='women' href='#info$women'class='grid_3 inline'>"; 
 										 echo get_image('image',1,$women); 
 										 echo "</a>";
 										 echo "<div style='display:none'><div id='info$women' class='grid_8 lightbox women'>";

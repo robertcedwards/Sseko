9a6d7ab308c501ddc2425b939e0c30c6dadfc3d2
Rob Edwards - iMac <robertcedwards@coactlab.com>
2011-10-12 17:27:00 -0700

    updating title alt etc.

--- meet.php
+++ meet.php
@@ -10,9 +10,9 @@ Template Name: Meet the Women
 		<article  id="post-<?php the_ID(); ?>">
 				<?php the_content(); ?>	
 				<h1><?php wp_title(' ','true','right'); ?></h1>						
-				<a class="grid_4 alpha" href="/university-bound" alt="University Bound"><img src="http://sseko.welovewebsit.es/wp-content/uploads/2011/10/0u.png" alt="University Bound" title="University Bound" /><img class="grid_4" src="http://sseko.welovewebsit.es/wp-content/uploads/2011/10/0z.png" alt="" /></a>
-				<a class="grid_4" href="/veteran"><img title="Full Time Veteran" src="http://sseko.welovewebsit.es/wp-content/uploads/2011/10/0s.png" alt="Full Time Veterans" title="Full Time Veterans" /><img src="http://sseko.welovewebsit.es/wp-content/uploads/2011/10/0x.png" alt="" /></a>
-				<a class="grid_4 omega" href="/sseko-graduates" alt="Sseko Graduates"><img title="Sseko Graduates" alt="Sseko Graduates" src="http://sseko.welovewebsit.es/wp-content/uploads/2011/10/0t.png" alt="" /><img src="http://sseko.welovewebsit.es/wp-content/uploads/2011/10/0y.png" alt="" /></a>
+				<a class="grid_4 alpha" href="/university-bound" alt="University Bound"><img src="http://sseko.welovewebsit.es/wp-content/uploads/2011/10/0u.png" alt="University Bound" title="University Bound" /><img src="http://sseko.welovewebsit.es/wp-content/uploads/2011/10/0z.png" alt="University Bound" title="University Bound" /></a>
+				<a class="grid_4" href="/veteran"><img title="Full Time Veteran" src="http://sseko.welovewebsit.es/wp-content/uploads/2011/10/0s.png" alt="Full Time Veterans" title="Full Time Veterans" /><img src="http://sseko.welovewebsit.es/wp-content/uploads/2011/10/0x.png" alt="Full Time Veterans" /></a>
+				<a class="grid_4 omega" href="/sseko-graduates" alt="Sseko Graduates"><img title="Sseko Graduates" alt="Sseko Graduates" src="http://sseko.welovewebsit.es/wp-content/uploads/2011/10/0t.png" alt="" /><img src="http://sseko.welovewebsit.es/wp-content/uploads/2011/10/0y.png" alt="Sseko Graduates" /></a>
 
 		<div class="clear"></div>	
 		</article>

3ec25ae7c9f8ce7a0f38a8292fb22fc8f2f58396
Rob Edwards - iMac <robertcedwards@coactlab.com>
2011-10-14 12:31:34 -0700

    moving javascript around, added mobile view-prot

--- header.php
+++ header.php
@@ -69,29 +69,11 @@
 	<meta name="DC.subject" content="Every Sandal has a story">
 	<meta name="DC.creator" content="WeCreativeAgency.com">
 	
-	<!--  Mobile Viewport meta tag
-	j.mp/mobileviewport & davidbcalhoun.com/2010/viewport-metatag 
-	device-width : Occupy full width of the screen in its current orientation
-	initial-scale = 1.0 retains dimensions instead of zooming out if page height > device height
-	maximum-scale = 1.0 retains dimensions instead of zooming in if page width < device width -->
-	<!-- Uncomment to use; use thoughtfully Here's another comment!
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
-	-->
-	
 	<link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/_/img/favicon.ico">
-	<!-- This is the traditional favicon.
-		 - size: 16x16 or 32x32
-		 - transparency is OK
-		 - see wikipedia for info on browser support: http://mky.be/favicon/ -->
-		 
 	<link rel="apple-touch-icon" href="<?php bloginfo('template_directory'); ?>/_/img/apple-touch-icon.png">
-	<!-- The is the icon for iOS's Web Clip.
-		 - size: 57x57 for older iPhones, 72x72 for iPads, 114x114 for iPhone4's retina display (IMHO, just go ahead and use the biggest one)
-		 - To prevent iOS from applying its styles to the icon name it thusly: apple-touch-icon-precomposed.png
-		 - Transparency is not recommended (iOS will put a black BG behind the icon) -->
 	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/_/js/blueberry.css">
 	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/_/js/fancybox/jquery.fancybox-1.3.4.css" type="text/css" media="screen" />
-
 	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
 
 <script> 
@@ -114,14 +96,6 @@ var ADAPT_CONFIG = {
   ]
 };
 </script> 
-<script src="<?php bloginfo('template_directory'); ?>/_/js/adapt.min.js"></script>
-<script src="<?php bloginfo('template_directory'); ?>/_/js/modernizr-1.7.min.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
-<script src="<?php bloginfo('template_directory'); ?>/_/js/jquery.blueberry.js"></script>
-<script>$(window).load(function() {$('.blueberry').blueberry();});</script>
-<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/_/js/fancybox/jquery.fancybox-1.3.4.pack.js"></script>
-<script type="text/javascript">$(document).ready(function() {$("a.inlineimg").fancybox({'hideOnContentClick': true});});</script>
-<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 
 	<?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
 

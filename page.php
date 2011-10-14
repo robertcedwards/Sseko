09604b67d928ed76f72fb20a64d277c1679e9775
Rob Edwards - iMac <robertcedwards@coactlab.com>
2011-10-14 14:31:21 -0700

    added grid

--- page.php
+++ page.php
@@ -1,8 +1,14 @@
+<?php
+/*
+Template Name: Seeko Story pages
+*/
+?>
+
 <?php get_header(); ?>
 
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 			
-		<article class="post" id="post-<?php the_ID(); ?>">
+		<article class="post grid_12" id="post-<?php the_ID(); ?>">
 
 			<h2><?php the_title(); ?></h2>
 

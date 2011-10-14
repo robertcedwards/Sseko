5d8eae3d51ab0cae18570b580124ef0c0f63888d
Rob Edwards - iMac <robertcedwards@coactlab.com>
2011-10-14 15:31:01 -0700

    namespace fixes

--- footer.php
+++ footer.php
@@ -38,7 +38,21 @@
 <script src="<?php bloginfo('template_directory'); ?>/_/js/jquery.blueberry.js"></script>
 <script>$(window).load(function() {$('.blueberry').blueberry();});</script>
 <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/_/js/fancybox/jquery.fancybox-1.3.4.pack.js"></script>
-<script type="text/javascript">$(document).ready(function() {$("a.inlineimg").fancybox({'hideOnContentClick': true});});</script>
+<script type="text/javascript">$(document).ready(function() {$("a.inlineimg").fancybox({'hideOnContentClick': true});   var timer;
+        $('#menu-top-nav li').hover(
+            function() {
+                if(timer){
+                    clearTimeout(timer);
+                    timer = null;
+                }
+                $(this).children('.sub-menu').fadeIn();
+            },
+            function() {
+                timer = setTimeout(function(){
+                    $('.sub-menu').fadeOut();
+                    }, 1000);
+                }
+           );});</script>
 
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 	

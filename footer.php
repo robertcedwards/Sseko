52198af6185e7a7c493bdb1a9f30273c2f3771e2
Rob Edwards - iMac <robertcedwards@coactlab.com>
2011-10-14 15:27:13 -0700

    adding fixes

--- footer.php
+++ footer.php
@@ -34,10 +34,6 @@
 <script src="<?php bloginfo('template_directory'); ?>/_/js/adapt.min.js"></script>
 <script src="<?php bloginfo('template_directory'); ?>/_/js/modernizr-1.7.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
-<script src="<?php bloginfo('template_directory'); ?>/_/js/jquery.blueberry.js"></script>
-<script>$(window).load(function() {$('.blueberry').blueberry();});</script>
-<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/_/js/fancybox/jquery.fancybox-1.3.4.pack.js"></script>
-<script type="text/javascript">$(document).ready(function() {$("a.inlineimg").fancybox({'hideOnContentClick': true});});</script>
 <script type="text/javascript">
 var $j = jQuery.noConflict();
     $j(document).ready(function(){
@@ -58,6 +54,11 @@ var $j = jQuery.noConflict();
            );
         });
 </script>
+<script src="<?php bloginfo('template_directory'); ?>/_/js/jquery.blueberry.js"></script>
+<script>$(window).load(function() {$('.blueberry').blueberry();});</script>
+<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/_/js/fancybox/jquery.fancybox-1.3.4.pack.js"></script>
+<script type="text/javascript">$(document).ready(function() {$("a.inlineimg").fancybox({'hideOnContentClick': true});});</script>
+
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 	
 </body>

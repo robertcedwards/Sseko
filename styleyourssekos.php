9466f1bf4284f99d14c3ff7091ae4f2393d342c2
Rob Edwards - iMac <robertcedwards@coactlab.com>
2011-10-24 19:03:57 -0700

    added paper

--- styleyourssekos.php
+++ styleyourssekos.php
@@ -8,6 +8,7 @@ Template Name: Style your Ssekos
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 			
 		<article  id="post-<?php the_ID(); ?>">
+		<img src="images/notepaper.png"style="float:left;"/>
 				<?php the_content(); ?>	
 				<h1><?php wp_title(' ','true','right'); ?></h1>					
 				<?php $styles = getFieldOrder('image'); 

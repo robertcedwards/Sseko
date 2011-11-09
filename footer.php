752316a8d5b7448c17fb1cf4f0aa9352685b0e44
Rob Edwards - Macbook Air <robertcedwards@coactlab.com>
2011-11-09 09:02:41 -0800

    order changes

--- footer.php
+++ footer.php
@@ -7,7 +7,6 @@
 	<?php wp_footer(); ?>
 <script src="<?php bloginfo('template_directory'); ?>/_/js/modernizr-1.7.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
-
 <script src="<?php bloginfo('template_directory'); ?>/_/js/jquery.blueberry.js"></script>
 <script>$(window).load(function() {$('.blueberry').blueberry();});</script>
 <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/_/js/fancybox/jquery.fancybox-1.3.4.pack.js"></script>

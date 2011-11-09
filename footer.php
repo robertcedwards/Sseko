a6296a357204da7942c4db4896cd0601bd5cda13
Rob Edwards - iMac <robertcedwards@coactlab.com>
2011-11-08 20:12:00 -0800

    on ready

--- footer.php
+++ footer.php
@@ -12,11 +12,8 @@
 <script src="<?php bloginfo('template_directory'); ?>/_/js/jquery.blueberry.js"></script>
 <script>$(window).load(function() {$('.blueberry').blueberry();});</script>
 <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/_/js/fancybox/jquery.fancybox-1.3.4.pack.js"></script>
-<script type="text/javascript">
-    jQuery(function($) {
-    $( "a.inlineimg" ).rlightbox();
-    });
-</script>
+<script type="text/javascript">$(document).ready(function() {$( "a.inline" ).rlightbox();
+});});</script>
 <script type="text/javascript">
     $(document).ready(function(){
         var timer;

76f0b719e49946f14807e9610e5cd9086852b9cc
Rob Edwards - iMac <robertcedwards@coactlab.com>
2011-11-08 21:03:33 -0800

    ok, how about lightboxme

--- footer.php
+++ footer.php
@@ -8,16 +8,8 @@
 <script src="<?php bloginfo('template_directory'); ?>/_/js/adapt.min.js"></script>
 <script src="<?php bloginfo('template_directory'); ?>/_/js/modernizr-1.7.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
-
 <script src="<?php bloginfo('template_directory'); ?>/_/js/jquery.blueberry.js"></script>
-<script>$(window).load(function() {$('.blueberry').blueberry();});</script>
-    <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/_/js/widget/lib/jquery.ui.core.min.js"></script>
-    <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/_/js/widget/lib/jquery.ui.widget.min.js"></script>
-    <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/_/js/widget/lib/jquery.ui.rlightbox.min.js"></script>
-    <link type="text/css" rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/_/js/widget/css/ui-lightness/jquery-ui-1.8.16.custom.css" />
-    <link type="text/css" rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/_/js/widget/css/lightbox.min.css" />
-
-<script type="text/javascript">$(document).ready(function() {$( ".lb_inlineimg" ).rlightbox();});</script>
+<script type="text/javascript">$(document).ready(function() {$( "a.inlineimg" ).rlightbox();});</script>
 <script type="text/javascript">
     $(document).ready(function(){
         var timer;

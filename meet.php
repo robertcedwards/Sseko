b8121274b7cfab90f8de8a5c33773aaefacc61ed
Rob Edwards - Macbook Air <robertcedwards@coactlab.com>
2011-11-10 09:18:33 -0800

    updating the meet page

--- meet.php
+++ meet.php
@@ -4,11 +4,11 @@ Template Name: Meet the Women
 */
 ?>
 <?php get_header(); ?>
-	<div id="content" class="grid_12">
+	<div id="content">
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 			
 		<article  id="post-<?php the_ID(); ?>" cla>
-				<?php the_content(); ?>	
+				<?php the_content('class="grid_12 alpha"'); ?>	
 				<h1><?php wp_title(' ','true','right'); ?></h1>						
 				<a class="grid_4 alpha" href="/university-bound" alt="University Bound"><img src="http://sseko.welovewebsit.es/wp-content/uploads/2011/10/0u.png" alt="University Bound" title="University Bound" /><img src="http://sseko.welovewebsit.es/wp-content/uploads/2011/10/0z.png" alt="University Bound" title="University Bound" /></a>
 				<a class="grid_4" href="/veteran"><img title="Full Time Veteran" src="http://sseko.welovewebsit.es/wp-content/uploads/2011/10/0s.png" alt="Full Time Veterans" title="Full Time Veterans" /><img src="http://sseko.welovewebsit.es/wp-content/uploads/2011/10/0x.png" alt="Full Time Veterans" /></a>

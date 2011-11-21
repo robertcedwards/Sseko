94cbd046b0070b204affeb97a763ba0bfde9d900
Robert C Edwards <redwards@coactlab.com>
2011-11-20 20:48:45 -0800

    added grid_12 back now that the margins from style.css have been removed

--- page.php
+++ page.php
@@ -8,7 +8,7 @@ Template Name: Seeko Story pages
 
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 			
-		<article class="post" id="post-<?php the_ID(); ?>">
+		<article class="post grid_12" id="post-<?php the_ID(); ?>">
 
 			<h2><?php the_title(); ?></h2>
 

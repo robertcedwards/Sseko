32ca8df6cce2591724993acc4b1c5ef4c4c86c0b
Robert C Edwards <redwards@coactlab.com>
2011-12-12 09:55:23 -0800

    adding text into notes

--- meet.php
+++ meet.php
@@ -12,7 +12,8 @@ Template Name: Meet the Women
 				<h1><?php wp_title(' ','true','right'); ?></h1>						
 				<a class="grid_4 alpha" href="/university-bound" alt="University Bound">
 					<img src="http://sseko.welovewebsit.es/wp-content/uploads/2011/11/0u.jpg" alt="University Bound" title="University Bound" class="grid_4" />
-					<img src="http://sseko.welovewebsit.es/wp-content/uploads/2011/10/0z.png" alt="University Bound" title="University Bound" class="grid_4" /></a>
+				</a>
+				<div class="grid_4" class="meetnote"><p>Our university bound team members have recently graduated from Sseko and work with us during the 9 months in between high school and university to earn money towards college.</p></div>
 				<a class="grid_4" href="/veteran">
 				<img title="Full Time Veteran" src="http://sseko.welovewebsit.es/wp-content/uploads/2011/11/0s.jpg" alt="Full Time Veterans" title="Full Time Veterans"class="grid_4" />
 				<img src="http://sseko.welovewebsit.es/wp-content/uploads/2011/10/0x.png" alt="Full Time Veterans"class="grid_4" /></a>

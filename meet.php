00ba6f346df36bf1e1399e4d8d788c0441e63a7e
Robert C Edwards <redwards@coactlab.com>
2011-12-12 10:00:34 -0800

    wrapping with picture and note

--- meet.php
+++ meet.php
@@ -10,16 +10,20 @@ Template Name: Meet the Women
 		<article  id="post-<?php the_ID(); ?>">
 				<?php the_content(); ?>	
 				<h1><?php wp_title(' ','true','right'); ?></h1>						
-				<a class="grid_4 alpha" href="/university-bound" alt="University Bound">
-					<img src="http://sseko.welovewebsit.es/wp-content/uploads/2011/11/0u.jpg" alt="University Bound" title="University Bound" class="grid_4" />
+				<div class="grid_4 alpha">
+				<a href="/university-bound" alt="University Bound">
+					<img src="/wp-content/uploads/2011/11/0u.jpg" alt="University Bound" title="University Bound" class="grid_4" />
 				</a>
-				<div class="grid_4" class="meetnote"><p>Our university bound team members have recently graduated from Sseko and work with us during the 9 months in between high school and university to earn money towards college.</p></div>
+				<br />
+				<div class="meetnote"><p>Our university bound team members have recently graduated from Sseko and work with us during the 9 months in between high school and university to earn money towards college.</p></div>
+				</div>
+
 				<a class="grid_4" href="/veteran">
 				<img title="Full Time Veteran" src="http://sseko.welovewebsit.es/wp-content/uploads/2011/11/0s.jpg" alt="Full Time Veterans" title="Full Time Veterans"class="grid_4" />
-				<img src="http://sseko.welovewebsit.es/wp-content/uploads/2011/10/0x.png" alt="Full Time Veterans"class="grid_4" /></a>
+				<img src="/wp-content/uploads/2011/10/0x.png" alt="Full Time Veterans"class="grid_4" /></a>
 				<a class="grid_4 omega" href="/sseko-graduates" alt="Sseko Graduates">
-				<img title="Sseko Graduates" alt="Sseko Graduates" src="http://sseko.welovewebsit.es/wp-content/uploads/2011/11/0t.jpg" class="grid_4"alt="" />
-				<img src="http://sseko.welovewebsit.es/wp-content/uploads/2011/11/0y.png" alt="Sseko Graduates"class="grid_4" /></a>
+				<img title="Sseko Graduates" alt="Sseko Graduates" src="/wp-content/uploads/2011/11/0t.jpg" class="grid_4"alt="" />
+				<img src="/wp-content/uploads/2011/11/0y.png" alt="Sseko Graduates"class="grid_4" /></a>
 
 		<div class="clear"></div>	
 		</article>

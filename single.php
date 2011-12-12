afbbad231992a9b36cfdba74d18a3f4fa65af7f9
joshfishman <thejoshfishman@gmail.com>
2011-12-11 17:51:56 -0800

    fixed author meta on single post page

--- single.php
+++ single.php
@@ -7,11 +7,17 @@
  		<article <?php post_class('grid_9') ?> id="post-<?php the_ID(); ?>">
 
             <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-            	<span class="entry-meta"><?php include (TEMPLATEPATH . '/_/inc/meta.php' ); ?></span>
-	        	<hr class="grid_4">
 	        <div class="clear"></div>
 			
 			<?php the_content(); ?>
+			<div id="authorbio"><h2>About the Author</h2><?php if ( get_the_author_meta( 'description' ) ) : // If a user has filled out their description, show a bio on their entries  ?>
+                                <?php echo get_avatar( get_the_author_meta( 'user_email' ), 60 ); ?>
+                           
+                                <?php the_author_meta( 'description' ); ?>
+                                <a href="<?php the_author_meta('user_url' );?>"><?php the_author_meta('user_url' );?><h4>View all posts by <?php the_author(); ?> </h4></a>
+                                <h3>Follow <?php the_author(); ?> on twitter</h3>
+                                <a href="http://twitter.com/<?php the_author_meta('twitter' );?>"><?php the_author_meta('twitter' );?></a>
+                                <?php endif; ?></div>
         	<hr class="grid_8">
  			<div class="clear"></div>
  		</article>

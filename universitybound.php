8b03b9f7c2662b6dd5059f3751809edcc1f22533
Robert C Edwards <redwards@coactlab.com>
2011-12-12 08:51:03 -0800

    reorder image and note

--- universitybound.php
+++ universitybound.php
@@ -12,11 +12,12 @@ Template Name: University Bound
 				<h1><?php wp_title(' ','true','right'); ?></h1>
 				<p></p>
 				<div id="pagenote" class="flat grid_3">
-					<img class="special" src="/wp-content/themes/Sseko/images/pagenote.jpg">
 					<div class="notetext">
 						<h2 class="notetitle">University Bound</h2>
 						<p>Our university bound team members have recently graduated high school and work with us during the 9 months in between high school and university to earn money towards college.</p>
 					</div>
+					<img class="special" src="/wp-content/themes/Sseko/images/pagenote.jpg">
+
 				</div>
 				<?php $womens = getFieldOrder('image'); 
 									if(is_array($womens))

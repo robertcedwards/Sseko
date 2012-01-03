bfc8da1d63a983ab2b8ecbf187f1f395d648ae93
joshfishman <thejoshfishman@gmail.com>
2012-01-03 11:51:00 -0800

    cleaning up 404 page try number 1

--- 404.php
+++ 404.php
@@ -1,7 +1,16 @@
 <?php get_header(); ?>
 
-	<h2><?php _e('Error 404 - Page Not Found','html5reset'); ?></h2>
+<div id="404" class="grid_9">	<h2>Sorry, the page you are looking for cannot be found. </h2>
+<h3>Here are some other things you can do</h3>
+<ul>
+<li><a href="http://shop.ssekodesigns.com">Shop for some new Ssekos</a></li
+<li><a href="http://ssekodesigns.com/style">Learn how to style your Ssekos</a></li>
+<li><a href="https://www.facebook.com/ssekodesigns">Like us on Facebook!</a></li>
+</ul>
 
-<?php get_sidebar(); ?>
+Feel free to contact us if you still need help
+</div>
+
+<div class="grid_3"><?php get_sidebar(); ?></div>
 
 <?php get_footer(); ?>

590e95608526b3c62928f9affa39da0bef80ec1d
Rob Edwards - iMac <robertcedwards@coactlab.com>
2012-01-17 11:23:55 -0800

    fixed error

--- functions.php
+++ functions.php
@@ -38,8 +38,8 @@
     if ( function_exists( 'register_nav_menus' ) ) {
     register_nav_menus(
         array(
-          'main_menu' => 'Main Menu'  
-          'footer_menu' => 'Footer Menu',
+          'main_menu' => 'Main Menu'  ,
+          'footer_menu' => 'Footer Menu'
         )
     );
 }
